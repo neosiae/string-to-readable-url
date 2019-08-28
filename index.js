0e71e3ac9e946563aa7775ba0be40d42ee42199a
Denis Kotlica <neosiae@protonmail.com>
2019-08-28 13:18:27 +0200

    Match all non-alphanumeric chars

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ module.exports = function(str) {
   if (typeof str !== "string") {
     throw new TypeError(`Expected string but got ${typeof str}`);
   }
-  return str
-    .toLowerCase()
-    .split(" ")
-    .join("-");
+  return str.toLowerCase().replace(/[^0-9a-z]/gi, "-");
 };
--- index.spec.js
+++ index.spec.js
@@ -10,9 +10,15 @@ test("returns an original string if there is only one word", t => {
 });
 
 test("transforms a string to the readable url", t => {
-  const url = stringToReadableURL("String to readable url");
+  const url1 = stringToReadableURL("String~to@readable#url");
+  const url2 = stringToReadableURL("String$to%readable^url");
+  const url3 = stringToReadableURL("String&to(readable)url");
+  const url4 = stringToReadableURL("String_to+readable=url");
 
-  t.equal("string-to-readable-url", url);
+  t.equal("string-to-readable-url", url1);
+  t.equal("string-to-readable-url", url2);
+  t.equal("string-to-readable-url", url3);
+  t.equal("string-to-readable-url", url4);
 
   t.end();
 });
